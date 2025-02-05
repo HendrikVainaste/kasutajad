d236946412389891023fdbc247aa70676bb3513f
HendrikVainaste <hendrik.vainaste@voco.ee>
2025-02-05 13:28:08 +0200

    Kasutaja kustutamine

---
+++ kustutamine.ps1
@@ -0,0 +1,17 @@
+## Loo uus skript, mis palub kasutaja sisestada kasutaja ees- ja perenimi, keda on vaja kustutada lokaalsete kasutajate hulgast.
+
+# Palub kasutajal sisestada eesnimi ja perenimi
+	$Eesnimi = Read-Host "Sisestage kasutaja eesnimi (ainult ladina tähed)"
+	$Perenimi = Read-Host "Sisestage kasutaja perenimi (ainult ladina tähed)"
+
+# Loob täisnime ees- ja perekonnanimest
+	$FullName = "$Eesnimi $Perenimi"
+
+# Leiab kasutajanime täisnime alusel (võib eeldada, et kasutajanimi on ees- ja perekonnanimi)
+	$Kasutajanimi = "$Eesnimi.$Perenimi"
+
+# Kustutab kohaliku kasutaja
+	Remove-LocalUser -Name $Kasutajanimi
+
+# Teavitab kasutajat, et konto on kustutatud
+	Write-Host "Kasutajakonto $FullName on edukalt kustutatud."
